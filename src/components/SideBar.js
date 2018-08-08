82457c1be3d90323f0999d8edf21b539037443c7
Deepak <deepakkumarsahoo231@gmail.com>
2018-08-08 14:59:56 +0530

    Initial commit

---
+++ src/components/SideBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const SideBar = (props) => {
+  return (
+    <div className="w3-sidebar w3-bar-block"> 
+        <a href="#" className="w3-bar-item w3-button">Link 1</a>
+        <a href="#" className="w3-bar-item w3-button">Link 2</a>
+        <a href="#" className="w3-bar-item w3-button">Link 3</a>
+        <a href="#" className="w3-bar-item w3-button">Link 1</a>
+        <a href="#" className="w3-bar-item w3-button">Link 2</a>
+        <a href="#" className="w3-bar-item w3-button">Link 3</a>
+        <a href="#" className="w3-bar-item w3-button">Link 1</a>
+        <a href="#" className="w3-bar-item w3-button">Link 2</a>
+        <a href="#" className="w3-bar-item w3-button">Link 3</a>
+        <a href="#" className="w3-bar-item w3-button">Link 1</a>
+        <a href="#" className="w3-bar-item w3-button">Link 2</a>
+        <a href="#" className="w3-bar-item w3-button">Link 3</a>
+        <a href="#" className="w3-bar-item w3-button">Link 1</a>
+        <a href="#" className="w3-bar-item w3-button">Link 2</a>
+        <a href="#" className="w3-bar-item w3-button">Link 3</a>
+        <a href="#" className="w3-bar-item w3-button">Link 1</a>
+        <a href="#" className="w3-bar-item w3-button">Link 2</a>
+        <a href="#" className="w3-bar-item w3-button">Link 3</a>
+        <a href="#" className="w3-bar-item w3-button">Link 1</a>
+        <a href="#" className="w3-bar-item w3-button">Link 2</a>
+        <a href="#" className="w3-bar-item w3-button">Link 3</a>
+        <a href="#" className="w3-bar-item w3-button">Link 1</a>
+        <a href="#" className="w3-bar-item w3-button">Link 2</a>
+        <a href="#" className="w3-bar-item w3-button">Link 3</a>
+        <a href="#" className="w3-bar-item w3-button">Link 1</a>
+        <a href="#" className="w3-bar-item w3-button">Link 2</a>
+        <a href="#" className="w3-bar-item w3-button">Link 3</a>
+    </div>
+  );
+}
+
+export default SideBar;
